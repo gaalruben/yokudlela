b7a61d41f44d7e8bf3c0578a80de6f382278eaa8
rubengaal <63323420+gaalruben@users.noreply.github.com>
2023-05-24 20:06:28 +0200

    gradle files

--- recipes/src/main/java/yokudlela/recipes/SpringApplication.java
+++ recipes/src/main/java/yokudlela/recipes/SpringApplication.java
@@ -1,11 +1,31 @@
 package yokudlela.recipes;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "yokudlela.recipes")
 @SpringBootApplication
 public class SpringApplication {
     public static void main(String[] args)
     {
         org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
