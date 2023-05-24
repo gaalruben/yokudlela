d2fc8c65f2a1d76755e34cf2552cb5b5839c724b
rubengaal <63323420+gaalruben@users.noreply.github.com>
2023-05-24 23:21:42 +0200

    initialized market mikroservice project

---
+++ market/src/main/java/yokudlela/market/SpringApplication.java
@@ -0,0 +1,29 @@
+package yokudlela.market;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringApplication {
+    public static void main(String[] args)
+    {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
